e593361339cf77c8c4fcae33b651fee83552fcd6
Niko Barateli <nbara15@freeuni.edu.ge>
2017-11-23 19:13:04 +0400

    extracted map script into  a separate js file

---
+++ www/js/maps.js
@@ -0,0 +1,47 @@
+var MyLat;
+var MyLong;
+
+function initMap() {
+
+    MyLat = 41.718287;
+    MyLong = 44.778728;
+
+    var myLatLng = {lat: MyLat, lng: MyLong};
+
+    gmap = new google.maps.Map(document.getElementById('gmap'), {
+        zoom: 18,
+        center: myLatLng,
+        zIndex: 70,
+
+        disableDefaultUI: true
+
+
+    });
+
+    var myicon = {
+        url: "pin.svg", // url
+        scaledSize: new google.maps.Size(30, 36), // size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(15, 18), // anchor
+        ratation: 30
+    };
+
+    var caricon = {
+        url: "car.svg", // url
+        scaledSize: new google.maps.Size(50, 50), // size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(25, 25), // anchor
+
+    };
+
+
+    MyMarker = new google.maps.Marker({
+        position: myLatLng,
+        map: gmap,
+        icon: myicon
+
+    });
+
+}
+
+//  mapTypeId: 'satellite',
