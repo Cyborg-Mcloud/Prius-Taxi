d229a1024b494d1f46833682b09299d4cdb64b47
Niko Barateli <nbara15@freeuni.edu.ge>
2017-12-14 13:57:50 +0400

    minor fix

--- www/js/maps.js
+++ www/js/maps.js
@@ -70,9 +70,9 @@ function initMap() {
     };
     var gpsIcon = {
         url: "resources/images/Clustericon.svg", // url
-        scaledSize: new google.maps.Size(50, 50), // size
+        scaledSize: new google.maps.Size(30, 30), // size
         origin: new google.maps.Point(0, 0), // origin
-        anchor: new google.maps.Point(49, 49) // anchor
+        anchor: new google.maps.Point(29, 29) // anchor
 
 
     };
