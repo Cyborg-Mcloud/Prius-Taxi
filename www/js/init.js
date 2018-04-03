3bd1710b9374a73106ac7799665a833bb65a98cb
Cyborg-Mcloud <uabashidze@gmail.com>
2018-04-03 10:21:07 +0400

    fdf
    ee

--- www/js/init.js
+++ www/js/init.js
@@ -334,12 +334,28 @@ function update_data() {
 						document.getElementById("driver_info").innerHTML =  taxiname;
                         //document.getElementById("taxi_number").innerHTML = "ბორტის ნომერი: " + taxiname;
                         if (notified == 0) {
-                            window.plugin.notification.local.schedule({
-                                id: "321123",  // A unique id of the notifiction
-                                text: "თქვენი ტაქსი მოვიდა და გელოდებათ",  // The message that is displayed
-                                title: "ტაქსი მოვიდა",  // The title of the message
 
-                            });
+							if (“Notification” in window) {
+							  Notification.requestPermission(function (permission) {
+								// If the user accepts, let’s create a notification
+								if (permission === ‘granted’) {
+								  var notification = new Notification(“ტაქსი მოვიდ”, {
+									   tag: ‘message1’, 
+									   body: “თქვენი ტაქსი მოვიდა და გელოდებათ” 
+								  }); 
+								  notification.onshow  = function() { console.log(‘show’); };
+								  notification.onclose = function() { console.log(‘close’); };
+								  notification.onclick = function() { console.log(‘click’); };
+								}
+							  });
+							}
+
+                        //    window.plugin.notification.local.schedule({
+                          //      id: "321123",  // A unique id of the notifiction
+                            //    text: "თქვენი ტაქსი მოვიდა და გელოდებათ",  // The message that is displayed
+                              //  title: "ტაქსი მოვიდა",  // The title of the message
+
+//                            });
                             notified = 1;
                         }
 
